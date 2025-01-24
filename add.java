38951fe3b071940d9afa2419fe425e47bd707f63
Aswath4 <aswathedu05@gmail.com>
2025-01-24 10:52:31 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class add {
+    public static void main(String args[])
+    {
+    Scanner asw = new Scanner(System.in);
+    System.out.print("Enter a Number:");
+    int N1 = asw.nextInt();
+    System.out.print("Enter a Number:");
+    int N2 = asw.nextInt();
+    int N3 = N1+N2;
+    System.out.println("The Sum is :"+ N3);
+    }
+
+   
+    
+}
