cfc104c742ae2e301d4b04920ead5c7eb93e2a88
Aswath4 <aswathedu05@gmail.com>
2025-01-24 11:25:44 +0530

    Add files via upload

---
+++ compare.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class compare
+{
+public static void main(String args[])
+{
+Scanner asw = new Scanner(System.in);
+System.out.print("Enter a Number:");
+int N1 = asw.nextInt();
+System.out.print("Enter a Number:");
+int N2 = asw.nextInt();
+System.out.println(N1==N2);
+System.out.println(N1!=N2);
+System.out.println(N1<=N2);
+System.out.println(N1>=N2);
+}
+}
+
+
+
+
