015c4fa9e8c4cdfd4745110fac99d41712d28fea
Aswath4 <aswathedu05@gmail.com>
2025-01-27 09:28:34 +0530

    Add files via upload

---
+++ Date_and_time.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Date_and_time
+{
+    public static void main(String args[])
+    {
+        Date Dt = new Date();
+        System.out.println("The current Date and Time is:" + Dt);
+        
+    }
+}
